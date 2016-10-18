25fb74d828e9146c98541d111d45aa4411ecc203
Damian <damianclluch@gmail.com>
2016-10-18 11:19:25 -0300

    act

--- web/js/contact_me.js
+++ web/js/contact_me.js
@@ -11,23 +11,23 @@ $(function() {
             event.preventDefault();
 
             // get values from FORM
-            var nombre = $("input#name").val();
+            var name = $("input#name").val();
             var email = $("input#email").val();
-            var telefono = $("input#phone").val();
-            var mensaje = $("textarea#message").val();
-            var nombrecheck = nombre; // For Success/Failure Message
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if (nombrecheck.indexOf(' ') >= 0) {
-                nombrecheck = nombre.split(' ').slice(0, -1).join(' ');
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
-                    nombre: nombre,
-                    telefono: telefono,
+                    name: name,
+                    phone: phone,
                     email: email,
-                    message: mensaje
+                    message: message
                 },
                 cache: false,
                 success: function() {
@@ -37,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Tu mensaje ha sido enviado </strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + nombrecheck + ", el servidor de correo no esta respondiendo, reintente nuevamente mas tarde");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
--- web/js/mail/contact_me.php
+++ web/js/mail/contact_me.php
@@ -1,22 +1,22 @@
 <?php
 // Check for empty fields
-if(empty($_POST['nombre'])  		||
+if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['telefono']) 		||
-   empty($_POST['mensaje'])	||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
 
-$name = strip_tags(htmlspecialchars($_POST['nombre']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['telefono']));
-$message = strip_tags(htmlspecialchars($_POST['mensaje']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'info@euterpeestudio.com.ar'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@euterpeestudio'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: info@euterpeestudio.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
