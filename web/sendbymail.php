d29bcd9eb5fc5989ed649d73ef258e7294f8b33e
Damian <damianclluch@gmail.com>
2016-10-18 12:02:27 -0300

    act

--- web/sendbymail.php
+++ web/sendbymail.php
@@ -1,60 +1,132 @@
 <?php
-
 if(isset($_POST['email'])) {
-    $email_to = "damianclluch@gmail.com";
-    $email_subject = "Contacto desde Web";
+
+    // Edita las dos líneas siguientes con tu dirección de correo y asunto personalizados
+
+    $email_to = "
+ info@euterpeestudio.com.ar";
+
+    $email_subject = "Asunto";
+
     function died($error) {
-        // mensajes de error
+
+        // si hay algún error, el formulario puede desplegar su mensaje de aviso
+
         echo "Lo sentimos, hubo un error en sus datos y el formulario no puede ser enviado en este momento. ";
+
         echo "Detalle de los errores.<br /><br />";
+
         echo $error."<br /><br />";
+
         echo "Porfavor corrija estos errores e inténtelo de nuevo.<br /><br />";
         die();
     }
+
     // Se valida que los campos del formulairo estén llenos
-    if(!isset($_POST['nombre']) ||
+
+    if(!isset($_POST['name']) ||
+
         !isset($_POST['email']) ||
-        !isset($_POST['telefono']) ||
-        !isset($_POST['mensaje'])) {
-        die('Lo sentimos pero parece haber un problema con los datos enviados.');
+
+        !isset($_POST['telephone']) ||
+
+        !isset($_POST['message'])) {
+
+        died('Lo sentimos pero parece haber un problema con los datos enviados.');
+
     }
- //En esta parte el valor "name"  sirve para crear las variables que recolectaran la información de cada campo
-    $first_name = $_POST['nombre']; // requerido
+ //En esta parte el valor "name" nos sirve para crear las variables que recolectaran la información de cada campo
+
+    $first_name = $_POST['name']; // requerido
+
     $email_from = $_POST['email']; // requerido
-    $telephone = $_POST['telefono']; // no requerido
-    $message = $_POST['mensaje']; // requerido
-    $error_message = "";//Linea numero 52;
+
+    $telephone = $_POST['telephone']; // no requerido
+
+    $message = $_POST['message']; // requerido
+
+    $error_message = "";
+
 //En esta parte se verifica que la dirección de correo sea válida
+
    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
   if(!preg_match($email_exp,$email_from)) {
+
     $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
+
   }
+
 //En esta parte se validan las cadenas de texto
+
     $string_exp = "/^[A-Za-z .'-]+$/";
+
   if(!preg_match($string_exp,$first_name)) {
+
     $error_message .= 'El formato del nombre no es válido<br />';
+
   }
+
   if(strlen($message) < 2) {
+
     $error_message .= 'El formato del texto no es válido.<br />';
+
   }
+
   if(strlen($error_message) > 0) {
-    die($error_message);
+
+    died($error_message);
+
   }
-//Este es el cuerpo del mensaje tal y como llegará al correo
+
+//A partir de aqui se contruye el cuerpo del mensaje tal y como llegará al correo
 
     $email_message = "Contenido del Mensaje.\n\n";
+
+
+
     function clean_string($string) {
+
       $bad = array("content-type","bcc:","to:","cc:","href");
+
       return str_replace($bad,"",$string);
+
     }
+
+
+
     $email_message .= "Nombre: ".clean_string($first_name)."\n";
+
     $email_message .= "Apellido: ".clean_string($last_name)."\n";
+
     $email_message .= "Email: ".clean_string($email_from)."\n";
+
     $email_message .= "Teléfono: ".clean_string($telephone)."\n";
+
     $email_message .= "Mensaje: ".clean_string($message)."\n";
+
+
 //Se crean los encabezados del correo
+
 $headers = 'From: '.$email_from."\r\n".
+
 'Reply-To: '.$email_from."\r\n" .
+
 'X-Mailer: PHP/' . phpversion();
+
 @mail($email_to, $email_subject, $email_message, $headers);
+
+?>
+
+
+
+<!-- incluye aqui tu propio mensaje de Éxito-->
+
+Gracias! Nos pondremos en contacto contigo a la brevedad
+
+
+<?php
+
+}
+
 ?>
